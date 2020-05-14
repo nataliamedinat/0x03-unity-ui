91d13529d75aaf695b2759db1a62ed2a55b6e16e
nataliamedinat <nataliamedinabotero@gmail.com>
2020-05-13 22:12:57 -0500

    Movement to the gabeobj

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    public Rigidbody rb;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetKey("w"))
+            rb.AddForce(0, 0, speed * Time.deltaTime);
+        if (Input.GetKey("s"))
+            rb.AddForce(0, 0, -speed * Time.deltaTime);
+        if (Input.GetKey("a"))
+            rb.AddForce(-speed * Time.deltaTime, 0, 0);
+        if (Input.GetKey("d"))
+            rb.AddForce(speed * Time.deltaTime, 0, 0);
+    }
+}
