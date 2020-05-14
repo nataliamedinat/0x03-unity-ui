579307098dadf91ee4ea9779a81070b3cb8740bf
nataliamedinat <nataliamedinabotero@gmail.com>
2020-05-14 02:11:42 -0500

    New prefab: Coin

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,19 +1,15 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
     public GameObject player;
-    private Vector3 offset;
-    // Start is called before the first frame update
-    void Start()
+    public Vector3 offset;
+    public Vector3 rotation;
+    private void Start()
     {
-        offset = transform.position - player.transform.position;
+        transform.Rotate(rotation);
     }
-
-    // Update is called once per frame
-    void LateUpdate()
+    void Update()
     {
         transform.position = player.transform.position + offset;
     }
