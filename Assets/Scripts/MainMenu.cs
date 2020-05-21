1932309548371b08d88abd022a2a8fe2c9d75924
nataliamedinat <nataliamedinabotero@gmail.com>
2020-05-21 15:50:07 -0500

    Script: Quit game + button

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -8,4 +8,10 @@ public class MainMenu : MonoBehaviour
     {
         SceneManager.LoadScene(0);
     }
+
+    public void QuitMaze()
+    {
+        Application.Quit();
+        Debug.Log("Quit game");
+    }
 }
